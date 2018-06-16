e7194b35ca624b22b2149caa6e755016b7b6555c
Jon Ahn <ahn.jon.s@gmail.com>
2018-06-16 04:14:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,13 @@ def summon_captain_planet(array)
   new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  i = 0
+  if array.any? {|x| x.length > 4}
+    return true
+  else
+    return false
+  i += 1
 end
 
 def find_the_cheese# code an argument here
